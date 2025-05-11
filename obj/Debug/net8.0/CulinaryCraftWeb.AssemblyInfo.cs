1a0356482d9f4abd175176520cbc91eaac9eec27
HudsonK86 <167348963+HudsonK86@users.noreply.github.com>
2025-05-12 01:23:04 +0800

    nothing

--- obj/Debug/net8.0/CulinaryCraftWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/CulinaryCraftWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CulinaryCraftWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4809544e52d3359a628d8233755d4ba8089905")]
 [assembly: System.Reflection.AssemblyProductAttribute("CulinaryCraftWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CulinaryCraftWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
